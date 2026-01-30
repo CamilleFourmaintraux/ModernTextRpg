28feb7cd0d80c7eb2dabcc39b6d6ba182c6206bb
CamilleFourmaintraux <fourmaintrauxcamille2004@gmail.com>
2026-01-30 14:44:12 +0100

    Add: Ajout des fichiers de base

---
+++ src/item/Armor.java
@@ -0,0 +1,6 @@
+package item;
+
+public class Armor extends Stuff{
+	int speed;
+	int stat;
+}
---
+++ src/item/Weapon.java
@@ -0,0 +1,7 @@
+package item;
+
+public class Weapon extends Stuff{
+	boolean longRange;
+	int atk;
+	int spd;
+}
